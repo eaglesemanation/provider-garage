2cc2e3a83e95c6b18ae5b152745ea0d8efd409b1
Vladimir Romashchenko <eaglesemanation@gmail.com>
2026-03-22 00:38:22 -0400

    Add access key id to connection details

--- config/cluster/key/config.go
+++ config/cluster/key/config.go
@@ -7,5 +7,15 @@ import (
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("garage_key", func(r *config.Resource) {
 		r.ShortGroup = "key"
+		r.Sensitive.AdditionalConnectionDetailsFn = func(attr map[string]any) (map[string][]byte, error) {
+			conn := map[string][]byte{}
+			if a, ok := attr["id"].(string); ok {
+				conn["access_key_id"] = []byte(a)
+			}
+			if a, ok := attr["secret_access_key"].(string); ok {
+				conn["secret_access_key"] = []byte(a)
+			}
+			return conn, nil
+		}
 	})
 }
